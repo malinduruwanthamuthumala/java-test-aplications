dc05f993e0a944ac85bf10084aa1c4b47460d62f
unknown <malinduruwantha@gmail.com>
2019-05-26 20:20:28 +0530

    print in new line

--- variables.java
+++ variables.java
@@ -1,7 +1,9 @@
 class variable{
 	public static void main(String args[]){
 	double a=4.56;
-	System.out.print(a);	
+	System.out.print(a);
+	System.out.println(" my name is");
+	System.out.print(a);
 
 	}
 }
