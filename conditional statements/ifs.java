6297e6675bffa6be7dc26079398c5f7629f517d6
unknown <malinduruwantha@gmail.com>
2019-05-27 21:28:39 +0530

    conditional statements

---
+++ conditional statements/ifs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class increments{
+
+
+Scanner mathsone=new Scanner(System.in);
+
+public static void main(String args[]){
+int girls,boys,people;
+girls=60;
+
+if(girls==6){
+System.out.print("girl is 60");
+
+}
+else{
+System.out.println("girl is not a girls");
+}
+
+}
+
+}
