ccb2a3a2bba579826cbe99d652af95a41257cc55
unknown <malinduruwantha@gmail.com>
2019-05-27 21:15:50 +0530

    math operators

---
+++ maths.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class mathsone{
+Scanner mathsone=new Scanner(System.in);
+
+public static void main(String args[]){
+int girls,boys,people;
+girls=6;
+boys=3;
+people=girls+boys;
+System.out.println(people);
+}
+
+}
