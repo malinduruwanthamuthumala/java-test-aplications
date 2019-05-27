a7959f3475ad639b168c13cd5c59b183ec368f81
unknown <malinduruwantha@gmail.com>
2019-05-27 21:52:22 +0530

    switch statements

---
+++ ifs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class increments{
+Scanner mathsone=new Scanner(System.in);
+
+public static void main(String args[]){
+int girls,boys,people;
+girls=6;
+boys=3;
+people=girls+boys;
+System.out.println(girls++);
+System.out.print(girls);
+girls+=10;
+System.out.println(girls);
+}
+
+}
---
+++ netbeans/JavaApplication4/src/switchpractice.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Malindu
+ */
+public class switchpractice {
+    
+
+
+public static void main(String args[]){
+int a =10;
+switch (a){
+    case 1:
+        System.out.print("you are a animal");
+        break;
+    case 10:
+        System.out.println("you are a vegetable");
+        break;
+}
+        
+}
+}
