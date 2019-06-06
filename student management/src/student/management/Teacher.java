589da93fdf3dd167a54feaf199cd42e67281d42c
unknown <malinduruwantha@gmail.com>
2019-06-06 21:42:49 +0530

    created teacher constructor

--- student management/src/student/management/Teacher.java
+++ student management/src/student/management/Teacher.java
@@ -13,4 +13,13 @@ public class Teacher {
    private int id;
    private String name;
    private int salary;
+   
+   public Teacher(int id,String name,int salary){
+       this.id=id;
+       this.name=name;
+       this.salary=salary;
+       
+   }
 }
+
+
